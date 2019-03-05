210d0da93ce91511cb0130a8f1b40596b7b39a5f
pelin <kayhanpelin@gmail.com>
2019-03-05 13:37:20 +0000

    Add the TV show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -14,23 +14,41 @@ class App extends Component {
         <div className="container">
           <div className="item">
             <img src={angelsInAmerica} alt="Angels in America" />
+            <div className="overlay">
+              <h2>Angels in America</h2>
+            </div>
           </div>
           <div className="item">
             <img src={boJackHorseman} alt="BoJack Horseman" />
+            <div className="overlay">
+              <h2>BoJack Horseman</h2>
+            </div>
           </div>
           <div className="item">
             <img src={mrRobot} alt="Mr Robot" />
+            <div className="overlay">
+              <h2>Mr Robot</h2>
+            </div>
           </div>
         </div>
         <div className="container">
           <div className="item">
             <img src={russianDoll} alt="Russian Doll" />
+            <div className="overlay">
+              <h2>Russian Doll</h2>
+            </div>
           </div>
           <div className="item">
             <img src={sixFeetUnder} alt="Six Feet Under" />
+            <div className="overlay">
+              <h2>Six Feet Under</h2>
+            </div>
           </div>
           <div className="item">
             <img src={westWorld} alt="Westworld" />
+            <div className="overlay">
+              <h2>Westworld</h2>
+            </div>
           </div>
         </div>
       </div>
